f38ca38094dfdec74375c0a1f7fdfc1bc01dcab1
ls3205 <ls3205@trevor.org>
2024-08-03 22:37:35 -0400

    Page title and icon

---
+++ src/app/icon.tsx
@@ -0,0 +1,35 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const size = {
+    width: 32,
+    height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    fontSize: 18,
+                    background: "#000",
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#22C55E",
+                    borderRadius: "5px",
+                }}
+            >
+                SPE
+            </div>
+        ),
+        {
+            ...size,
+        },
+    );
+}
